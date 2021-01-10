77708d5839cd12a52107486af7b642b2fefaaa07
Alice Wu <xiw451@ucsd.edu>
2021-01-10 20:54:33 +0800

    burger object calls orm functions

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS burger_db;
+
 CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
-	id INT AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR( 255 ) NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY ( id )
+	PRIMARY KEY (id)
 );
 
 
