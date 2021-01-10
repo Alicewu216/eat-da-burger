85852db7faf3473b9e937607673e16111c362bc6
Alice Wu <xiw451@ucsd.edu>
2021-01-10 19:40:23 +0800

    cmv structure set uo

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("chese burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Shack", false);
